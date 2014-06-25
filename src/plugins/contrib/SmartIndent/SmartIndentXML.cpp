Distrotech/codeblocks

commit 103518449a5c841e0ade048b8e4fd54203efb205

* SmartIndent: handle (some) smart indentation for embedded languages (PHP, JavaScript, etc) within HTML
git-svn-id: svn://svn.code.sf.net/p/codeblocks/code/trunk@9822 2a5c6006-c6dd-42ca-98ab-0921f2732cef

src/plugins/contrib/SmartIndent/SmartIndentXML.cpp
@@ -66,8 +66,9 @@ void SmartIndentXML::OnEditorHook(cbEditor* ed, wxScintillaEvent& event) const
 
     if (BraceCompletionEnabled())
     {
-        // finish tag
-        if ( ch == wxT('>') && !stc->IsString(stc->GetStyleAt(pos)) )
+        int curSty = stc->GetStyleAt(pos);
+        // finish XML/HTML tag
+        if ( ch == wxT('>') && !stc->IsString(curSty) )
         {
             wxString tag;
             for (int i = pos - 2; i > 0; --i)
@@ -122,6 +123,11 @@ void SmartIndentXML::OnEditorHook(cbEditor* ed, wxScintillaEvent& event) const
             if (tag != wxT("]]>"))
                 stc->InsertText(pos, wxT("]]>"));
         }
+        // embedded languages
+        else if (complQuote && curSty >= wxSCI_HJ_START && curSty <= wxSCI_HPHP_OPERATOR && !stc->IsString(curSty))
+        {
+            stc->DoBraceCompletion(ch);
+        }
     }
     // indent
     if (   AutoIndentEnabled()
@@ -163,6 +169,19 @@ void SmartIndentXML::OnEditorHook(cbEditor* ed, wxScintillaEvent& event) const
                         Indent(stc, indent);
                     }
                 }
+            }
+            else if (stc->GetStyleAt(pos) >= wxSCI_HJ_START && stc->GetStyleAt(pos) <= wxSCI_HPHP_OPERATOR)
+            {
+                // embedded language, indent braces
+                wxString lineSuffix = stc->GetLine(currLine).Strip(wxString::both);
+                if (lineSuffix == wxT('}') || lineSuffix == wxT(']'))
+                {
+                    stc->InsertText(pos, indent);
+                    stc->NewLine();
+                    Indent(stc, indent);
+                }
+                else if (GetLastNonWhitespaceChar(ed) == wxT('{'))
+                    Indent(stc, indent);
             }
             else
             {

commit 0875ab0c79e171a90d5305c6d0b9ca820e89b637

- SmartIndent: resolve crash candidate introduced in r9822; from within PHP, typing '{' on a blank line, then pressing enter causes a crash in (wx)Scintilla (backtrace gives no clue of why)
git-svn-id: svn://svn.code.sf.net/p/codeblocks/code/trunk@9833 2a5c6006-c6dd-42ca-98ab-0921f2732cef

src/plugins/contrib/SmartIndent/SmartIndentXML.cpp
@@ -173,11 +173,10 @@ void SmartIndentXML::OnEditorHook(cbEditor* ed, wxScintillaEvent& event) const
             else if (stc->GetStyleAt(pos) >= wxSCI_HJ_START && stc->GetStyleAt(pos) <= wxSCI_HPHP_OPERATOR)
             {
                 // embedded language, indent braces
-                wxString lineSuffix = stc->GetLine(currLine).Strip(wxString::both);
+                const wxString lineSuffix = stc->GetLine(currLine).Strip(wxString::both);
                 if (lineSuffix == wxT('}') || lineSuffix == wxT(']'))
                 {
-                    stc->InsertText(pos, indent);
-                    stc->NewLine();
+                    stc->InsertText(pos, GetEOLStr(stc->GetEOLMode()) + indent);
                     Indent(stc, indent);
                 }
                 else if (GetLastNonWhitespaceChar(ed) == wxT('{'))